1ee8f2194f9e7f8ebe4ce8f174b19fab55f689ee
jtanuja131 <jtanuja131@gmail.com>
2026-09-11 15:20:54 +0530

    Add CheckingAccount class with basic operations
    Implemented CheckingAccount class with deposit, withdraw, and statement methods.

---
+++ Banking.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class CheckingAccount
+{
+private:
+    string name;
+    int accountNumber;
+    double balance;
+    double transactionFee;
+
+public:
+
+    // Constructor
+    CheckingAccount(string n, int accNo, double bal, double fee)
+    {
+        name = n;
+        accountNumber = accNo;
+        balance = bal;
+        transactionFee = fee;
+    }
+
+    // Deposit
+    void deposit(double amount)
+    {
+        balance = balance + amount;
+
+        cout << "Deposited: " << amount << endl;
+    }
+
+    // Withdrawal
+    void withdraw(double amount)
+    {
+        double total = amount + transactionFee;
+
+        if (total <= balance)
+        {
+            balance = balance - total;
+
+            cout << "Withdrawn: " << amount << endl;
+            cout << "Transaction Fee: " << transactionFee << endl;
+            cout << "Total Deducted: " << total << endl;
+        }
+        else
+        {
+            cout << "Insufficient Balance!" << endl;
+        }
+    }
+
+    // Account Statement
+    void statement()
+    {
+        cout << "\n----- CHECKING ACCOUNT STATEMENT -----" << endl;
+
+        cout << "Name: " <<
